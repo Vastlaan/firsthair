a649735f2da76ec9d74a401d654a1200d30803f0
vastlaan <vastlaan@mail.com>
2021-03-23 13:38:29 +0100

    created draws of pages: afspraak, behandelingen, maatregelen

---
+++ pages/behandelingen.js
@@ -0,0 +1,10 @@
+import Layout from "../src/layout";
+import Contact from "../src/components/contact_page";
+
+export default function BehandelingenPage() {
+    return (
+        <Layout>
+            <Contact />
+        </Layout>
+    );
+}
---
+++ pages/maatregelen.js
@@ -0,0 +1,10 @@
+import Layout from "../src/layout";
+import Contact from "../src/components/contact_page";
+
+export default function MaatregelenPage() {
+    return (
+        <Layout>
+            <Contact />
+        </Layout>
+    );
+}

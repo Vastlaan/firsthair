008905206e954d512cac075e7530185efc2451ac
vastlaan <vastlaan@mail.com>
2021-03-23 13:24:39 +0100

    add link to logo

--- src/components/navigation/logo/index.js
+++ src/components/navigation/logo/index.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import { Logo } from "../../../styles";
 
 export default function LogoComponent() {
     return (
-        <Logo>
-            <img src="./img/logo_first_hair_white.svg" alt="logo first hair" />
-        </Logo>
+        <Link href="/">
+            <Logo>
+                <img
+                    src="./img/logo_first_hair_white.svg"
+                    alt="logo first hair"
+                />
+            </Logo>
+        </Link>
     );
 }
